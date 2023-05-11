814e53a822735c94c379a8bbc75346be779c6d55
martinBuder <martin.buder@designyourstage.com>
2023-05-11 06:50:08 +0200

    work at ArrayName startQuizHtml

--- script.js
+++ script.js
@@ -1,19 +1,91 @@
-let htmlAsk = [
+const htmlAsk = [
 	{"question": 'Wer hat HTML erfunden?',
 	"answer0": ['Robbie Williams'],
 	"answer1": ['Lady Gaga'],
 	"answer2": ['Tim Berners - Lee', 'true'],
 	"answer3": ['Justin Bieber']
 	},
-];// idee if includes true background green else red
+	{"question": 'Was bedeutet HTML?',
+	"answer0": ['Hypertext Markup Language', true],
+	"answer1": ['Nudelgericht'],
+	"answer2": ['ein Test'],
+	"answer3": ['Automarke']
+	},
+	{"question": 'Wie viele Buchstaben hat HTML',
+	"answer0": ['5'],
+	"answer1": ['2'],
+	"answer2": ['ca. 25'],
+	"answer3": ['4', true]
+	},
+];
 
+var cssAsk = [{"question": 'Wer hat CSS erfunden?',
+"answer0": ['Robbie Williams'],
+"answer1": ['Lady Gaga'],
+"answer2": ['Tim Berners - Lee', 'true'],
+"answer3": ['Justin Bieber']
+},
+{"question": 'Was bedeutet CSS?',
+"answer0": ['Hypertext Markup Language', true],
+"answer1": ['Nudelgericht'],
+"answer2": ['ein Test'],
+"answer3": ['Automarke']
+},
+{"question": 'Wie viele Buchstaben hat CSS',
+"answer0": ['5'],
+"answer1": ['2'],
+"answer2": ['ca. 25'],
+"answer3": ['4', true]
+},
+];
 
-let cssAsk = [];
-let jsAsk = [];
-let javaAsk = [];
+var jsAsk = [{
+"question": 'Wer hat Javascript erfunden?',
+"answer0": ['Robbie Williams'],
+"answer1": ['Lady Gaga'],
+"answer2": ['Tim Berners - Lee', 'true'],
+"answer3": ['Justin Bieber']
+},
+{"question": 'Was bedeutet Javascript?',
+"answer0": ['Hypertext Markup Language', true],
+"answer1": ['Nudelgericht'],
+"answer2": ['ein Test'],
+"answer3": ['Automarke']
+},
+{"question": 'Wie viele Buchstaben hat Javascript',
+"answer0": ['5'],
+"answer1": ['2'],
+"answer2": ['ca. 25'],
+"answer3": ['10', true]
+},
+];
+
+var javaAsk = [
+	{"question": 'Wer hat Java erfunden?',
+	"answer0": ['Robbie Williams'],
+	"answer1": ['Lady Gaga'],
+	"answer2": ['Tim Berners - Lee', 'true'],
+	"answer3": ['Justin Bieber']
+	},
+	{"question": 'Was bedeutet Java?',
+	"answer0": ['Hypertext Markup Language', true],
+	"answer1": ['Nudelgericht'],
+	"answer2": ['ein Test'],
+	"answer3": ['Automarke']
+	},
+	{"question": 'Wie viele Buchstaben hat Java',
+	"answer0": ['5'],
+	"answer1": ['2'],
+	"answer2": ['ca. 25'],
+	"answer3": ['4', true]
+	},
+];
 
 let content;
 let counter = 0;
+
+let AUDIO_SUCCESS = new Audio('audio/success.mp3');
+let AUDIO_FAIL = new Audio('audio/fail.mp3');
 
 function render() {
 	content = document.getElementById('content');
@@ -29,52 +101,49 @@ function firstContentHtml() {
 			</div>`
 }
 
-function renderHtmlAsk() {
-	content.innerHTML = askHtml('Html');
+function renderQuizStarting(Quiz) {
+	content.innerHTML = askStartingHtml(Quiz);
 }
 
-function renderCssAsk() {
-	content.innerHTML = askHtml('Css');
-}
+function askStartingHtml(Quiz) {
 
-function renderJsAsk() {
-	content.innerHTML = askHtml('Js');
-}
-
-function renderJavaAsk() {
-	content.innerHTML = askHtml('Java');
-}
-
-function askHtml(Quiz) {
 	return /*html */ `
 			<h2>Welcome to<br>
 					The Awesome <span class="uppercase">${Quiz}</span> Quiz</h2>
 				<p>Ready for the Challange?</p>
-				<button type="button" class="btn btn-lg" onclick="startQuiz${Quiz}()">START NOW</button>
+				<button type="button" class="btn btn-lg" onclick="startQuizHtml('${Quiz}')">START NOW</button>
 			</div>`
 }
 
-function startQuizHtml() { //ist kein html code 
-	content.innerHTML = quizHtml(); 
+function startQuizHtml(Quiz) { //ist kein html code 
+
+	let quizAsk = window[Quiz.toLowerCase() + 'Ask']
+	alert(quizAsk)
+	alert((Quiz.toLowerCase() + 'Ask').length)
+	let quizArray = window[Quiz.toLowerCase() + 'Ask'];
+	alert(quizArray)
+	for (let i = 0; i < quizArray.length; i++) {
+			content.innerHTML = quizHtml(quizArray, i);
+	}
 }
 
-function quizHtml() {
-
+function quizHtml(quizArray, i) {
+alert(quizArray)
 	return /*html*/ `
 	<div class="quizcard">
   <div class="card-body">
-    <h5 class="card-title">${htmlAsk[0]['question']}</h5>
+    <h5 class="card-title">${quizArray[i]['question']}</h5>
   </div>
   <ul class="list-group list-group-flush">
 			<div id="answers"></div>
-    <li class="list-group-item" id="answer0" onclick="checkAnswer(0)">${htmlAsk[0]['answer0'][0]}</li>
-    <li class="list-group-item" id="answer1" onclick="checkAnswer(1)">${htmlAsk[0]['answer1'][0]}</li>
-    <li class="list-group-item" id="answer2" onclick="checkAnswer(2)">${htmlAsk[0]['answer2'][0]}</li>
-				<li class="list-group-item" id="answer3" onclick="checkAnswer(3)">${htmlAsk[0]['answer3'][0]}</li>
+    <li class="list-group-item" id="answer0" onclick="checkAnswer(i)">${quizArray[i]['answer0'][0]}</li>
+    <li class="list-group-item" id="answer1" onclick="checkAnswer(1)">${quizArray[i]['answer1'][0]}</li>
+    <li class="list-group-item" id="answer2" onclick="checkAnswer(2)">${quizArray[i]['answer2'][0]}</li>
+				<li class="list-group-item" id="answer3" onclick="checkAnswer(3)">${quizArray[i]['answer3'][i]}</li>
   </ul>
   <div class="card-body">
-    <a href="#" class="card-link"><</a>
-    <a href="#" class="card-link">></a>
+    <button href="#" class="card-link"><</a>
+    <button href="#" class="card-link" id="nextAsk" disabled onclick="startQuizHtml('${Quiz}')">></button>
   </div>
 </div>`;
 }
@@ -84,9 +153,33 @@ function checkAnswer(i) {
 
 	if (htmlAsk[0][`answer${i}`].includes('true')) {
 		document.getElementById(`answer${i}`).classList.add('green');
-		counter++
+		AUDIO_SUCCESS.play();
+		counter++;
+		answers.classList.add('noOnclick')
 	} else {
-		document.getElementById(`answer${i}`).classList.add('red');
+		document.getElementById(`answer${i}`).	classList.add('red');
+		AUDIO_FAIL.play();
+
+			for (let j = 0; j < 4; j++) {
+				if (htmlAsk[0][`answer${j}`].includes('true')) {
+					document.getElementById(`answer${j}`).classList.add('green');
+					answers.classList.add('noOnclick')
+			}
 	}
 	answers.classList.add('noOnclick')
+	document.getElementById('nextAsk').disabled = false;
 }
+}
+
+// var testAsk = [1, 2, 2, 5, 1]
+
+// function startTest() {
+// 	startQuizHtml('Test')
+// }
+
+// function startQuizHtml(Maus) { 
+// 	let quizName = window[Maus.toLowerCase() + 'Ask'];
+// 	alert(quizName.length)
+// 	alert(quizName);
+// }
+
